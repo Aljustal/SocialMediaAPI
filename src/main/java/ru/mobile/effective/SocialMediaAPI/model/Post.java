31e044ab5bfaace46d9360195474407d5466ac43
Aljustal <orbit.fsk@yandex.ru>
2023-05-30 18:07:16 +0300

    add base models

---
+++ src/main/java/ru/mobile/effective/SocialMediaAPI/model/Post.java
@@ -0,0 +1,17 @@
+package ru.mobile.effective.SocialMediaAPI.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    private String title;
+}
---
+++ src/main/java/ru/mobile/effective/SocialMediaAPI/model/User.java
@@ -0,0 +1,21 @@
+package ru.mobile.effective.SocialMediaAPI.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String email;
+
+    private String password;
+}
