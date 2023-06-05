8e4c4ea24745b49d66e34a1236f9f37cdf9d6e32
Aljustal <orbit.fsk@yandex.ru>
2023-06-05 20:40:27 +0300

    add jsonwebtoken dependencies

--- src/main/java/ru/mobile/effective/SocialMediaAPI/SocialMediaApiApplication.java
+++ src/main/java/ru/mobile/effective/SocialMediaAPI/SocialMediaApiApplication.java
@@ -2,8 +2,10 @@ package ru.mobile.effective.SocialMediaAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ru.mobile.effective.SocialMediaAPI")
 public class SocialMediaApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ru/mobile/effective/SocialMediaAPI/controller/WelcomeController.java
+++ src/main/java/ru/mobile/effective/SocialMediaAPI/controller/WelcomeController.java
@@ -2,11 +2,13 @@ package ru.mobile.effective.SocialMediaAPI.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
     @GetMapping("/welcome")
-    public void welcome() {
-        System.out.println("welcome");
+    @ResponseBody
+    public String welcome() {
+        return "welcome";
     }
 }
